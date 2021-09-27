35e738f7a31e5e57e4e90af261bfb40072e67c64
MuradAbuArqoub <muradaboarkob2000@gmail.com>
2021-09-27 08:38:24 +0300

    need fixing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
+// import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './Footer';
+import BestBooks from './BestBooks.js';
+import Login from './Login.js'
+import Profile from './components/Profile.js'
+import { withAuth0 } from "@auth0/auth0-react";
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,19 +16,27 @@ import {
 class App extends React.Component {
 
   render() {
-    console.log('app', this.props);
-    return(
+    const { isAuthenticated } = this.props.auth0;
+    return (
       <>
         <Router>
           {/* <IsLoadingAndError> */}
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-              </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-            </Switch>
-            <Footer />
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+
+              {isAuthenticated ? <BestBooks/> : <Login/>}
+            </Route>
+
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+
+            <Route path="/profile">
+              <Profile/>
+            </Route>
+
+          </Switch>
+          <Footer />
           {/* </IsLoadingAndError> */}
         </Router>
       </>
